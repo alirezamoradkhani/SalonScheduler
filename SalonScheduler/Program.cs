c64fba4a8b3b290cb7112f290fe761e1467c4212
Alireza moradkhani <moradkhani1386@gmail.com>
2026-06-15 12:12:55 +0330

    feature():add swagger

--- SalonScheduler/Program.cs
+++ SalonScheduler/Program.cs
@@ -3,24 +3,27 @@ using SalonScheduler.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ۱. فعالسازی اتصال دیتابیس SQL Server به همراه اطلاعات کانکشن موجود در appsettings
 builder.Services.AddDbContext<SalonDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
-// اضافه کردن مستندات اختیاری API
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 
-// ۲. فعال‌سازی قابلیت لود سایت استاتیک (HTML, CSS, JS) از پوشه wwwroot
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
